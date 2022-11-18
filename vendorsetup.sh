c4cd56b9ce0882062330315ba304a4da973e386a
chingsky-liu <ivan33@163.com>
2022-11-18 16:07:39 +0000

    vendor: Rebrand to Project-Telescopium
    Change-Id: I889634baadae4d7722b6429e1443781780acfb46

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,3 @@
-lunch_others_targets=()
-for d in $(python vendor/aosp/tools/get_official_devices.py)
-do
-    for v in user userdebug eng; do
-        lunch_others_targets+=("aosp_$d-$v")
-    done
+for var in user userdebug eng; do
+    lunch_others_targets+=("aosp_$device-$var")
 done
